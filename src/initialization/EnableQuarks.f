ace3f751b908b80e6ff4f27e74a17c8b2f002545
Giovanni Stagnitto <giovanni.stagnitto@gmail.com>
2021-11-24 16:21:16 +0100

    fixing q2th in default + adding another wrapper function for C interface + adding two runcards for tests

--- src/initialization/EnableQuarks.f
+++ src/initialization/EnableQuarks.f
@@ -19,3 +19,29 @@
 *
       return
       end
+************************************************************************
+*     Easy the C interface by using a int
+*     0 = no quarks, 1 = yes quarks
+************************************************************************      
+      subroutine EnableQuarksInt(quarksinint)
+*
+      implicit none
+*
+      include "../commons/activeflavours.h"
+**
+*     Input Variables
+*
+      integer quarksinint
+*     
+      if (quarksinint.eq.0) then
+         quarks = .false.
+      elseif (quarksinint.eq.1) then
+         quarks = .true.
+      else
+         write(6,*) "In EnableQuarks.f:"
+         write(6,*) "Invalid value"
+         call exit(-10)
+      endif 
+*
+      return
+      end
