b896cbfb2993a10e62306f79eb0a709b78b02709
Giovanni Stagnitto <giovanni.stagnitto@gmail.com>
2021-11-24 11:28:00 +0100

    adding function with int argument to set flavour scheme

--- src/initialization/SetFlavourScheme.f
+++ src/initialization/SetFlavourScheme.f
@@ -19,3 +19,31 @@
 *
       return
       end
+************************************************************************
+*     Easy the C interface by using a int
+*     0 = FFNS, 1 = VFNS
+************************************************************************      
+      subroutine SetFlavourSchemeInt(nsinint)
+*
+      implicit none
+*     
+      include "../commons/ns.h"
+**
+*     Input Variables
+*     
+      integer nsinint
+*     
+      if (nsinint.eq.0) then
+         ns = "FFNS"
+      elseif (nsinint.eq.1) then
+         ns = "VFNS"
+      else
+         write(6,*) "In SetFlavourScheme.f:"
+         write(6,*) "Invalid value"
+         call exit(-10)
+      endif 
+*
+      return
+      end
+
+      
