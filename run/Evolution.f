3d55fa54f048c700f61c727e4b00af115946e1c9
vbertone <valerio.bertone@gmail.com>
2021-11-26 17:52:07 +0100

    small adjustment

--- run/Evolution.f
+++ run/Evolution.f
@@ -32,7 +32,7 @@
       call SetNFmax(8)
       call SetNFFN(8)
       call SetAlpha(0.0078152650036d0, 91.187600000000d0)
-      call EnableQuarks(.false.)
+      call EnableQuarks(.true.)
       call SetThresholds(0.000510998928d0,0.00216d0,0.00467d0,
      1     0.093d0,0.10566d0,1.27d0,1.77686d0,4.18d0,172.76d0)
 *
